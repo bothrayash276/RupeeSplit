b075ac2ff1a755013aba54bc7d316646c7f5b602
bothrayash276 <bothrayash276@gmail.com>
2026-01-29 22:03:03 +0530

    linked group with mongodb

---
+++ app/(user)/groups/_groupFxn.js
@@ -0,0 +1,22 @@
+export async function newGroup(data) {
+    // url
+    const url = 'http://localhost:8080/newgrp';
+    await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+}
+
+export async function updateUser(user) {
+    const url = 'http://localhost:8080/update'
+    fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type' : 'application/json'
+          },
+          body: JSON.stringify(user)
+        })
+}
--- app/(user)/groups/page.js
+++ app/(user)/groups/page.js
@@ -1,9 +1,178 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+import {newGroup, updateUser} from './_groupFxn'
+import serverData_User from '@/app/_data'
+import { useSession } from 'next-auth/react'
 
 const Groups = () => {
+  // Checks Loading
+  const [loading, setLoading] = useState(true)
+
+  // Save userData
+  const [user, setUser] = useState()
+
+  // State for popup of creating a new group
+  const [groupPopup, setGroupPopup] = useState(false)
+
+  // Empty Group Name Error
+  const [grpNameError, setGrpNameError] = useState(false)
+
+  // Loading Session
+  const {data:session, status} = useSession()
+
+  // Loading Userdata
+  useEffect( () => {
+    const load = async () => {
+      if( status === 'authenticated' ) {
+        try {
+          const data = await serverData_User(session.user.email)
+          setUser(data)
+        }
+        finally {
+          setLoading(false)
+        }
+      }
+    }
+    load()
+  }, [status])
+
+  // Function creates a group when button is pressed
+  const createGroup = () => {
+
+    // Initial Check if Name entered is empty
+    if (document.getElementById('groupName-Laptop')?.value === "" || document.getElementById('groupName-Mobile')?.value === "")
+    {    
+      setGrpNameError(true)
+      setTimeout(() => {
+        setGrpNameError(false)
+      }, 2000);
+      return
+    }
+    
+
+    // Gets Group Name
+    let groupName;
+    if(document.getElementById('groupName-Laptop')?.value){
+      groupName = document.getElementById('groupName-Laptop').value
+    }
+
+    // Group Template
+    const groupInfo = {
+      'id': uuidv4(),
+      'groupName': groupName,
+      'members': [user.uid],
+      'transactions' : []
+    }
+    // Excludes _id from rest of data to avoid conflict with MongoDB
+    const {_id, ...noIDuser} = user;
+
+    // Creating New User with updated groups array
+    const newUser = {
+      ...noIDuser,
+      'groups' : [...user.groups, groupInfo.id]
+    }
+    // Saving new user's data into the old user variable
+    setUser(newUser)
+    
+    // Updating the User Database
+    updateUser(newUser).finally(() => {})
+    
+    // Adding the new group entry to database
+    newGroup(groupInfo).finally(() => {setGroupPopup(false)})
+  }
+
+  // Loading Screen
+  if(loading){
+    return <>Loading....</>
+  }
+
+  // PopUp of Obtaining a Group Name
+  if(groupPopup){
+    return (
+      <>
+      {/* Creates a new group popup */}
+      <div className="mt-[25vh] w-full flex items-center justify-center">
+      <div 
+      className="bg-white flex flex-col items-center justify-center gap-5 p-8 rounded-xl">
+
+        {/* Title */}
+        <span
+        className='text-3xl font-bold'>
+          Create a New Group
+        </span>
+
+        {/* Input */}
+        <div className="w-full flex flex-col gap-2">
+
+          {/* Label */}
+          Name
+
+          {/* Input Box */}
+          <input
+          id='groupName-Laptop' 
+          placeholder='Enter a Group Name'
+          type="text"
+          className={`w-full px-3 py-2 rounded-2xl border  bg-gray-100 ${grpNameError ? "border-red-500" : "border-white" }`} />
+        
+          <span
+          className={`text-red-500 ${grpNameError ? "" : "hidden"}`}>
+            Group Name must to entered
+          </span>
+        </div>
+
+        {/* Create Button */}
+        <button
+        onClick={createGroup}
+        className='bg-[#2C9986] text-white px-10 py-2 rounded-full'>
+          Create
+        </button>
+
+      </div>
+      </div>
+      </>
+    )
+  }
+
   return (
     <>
-    Page showing all the groups
+    {/* Laptop UI */}
+    <div 
+    className={`not-md:hidden h-full w-full my-10 ${groupPopup ? "blur" : ""}`}>
+
+      {/* Container for Heading, Description and Create Group Button */}
+      <div 
+      className="w-full flex px-10">
+
+        {/* Heading and Description */}
+        <div
+        className='flex flex-col flex-1 gap-2'>
+
+          {/* Heading */}
+          <span
+          className='text-3xl font-bold'>
+            Groups
+          </span>
+          <span
+          className='text-gray-400 font-bold' >
+          Manage your shared expenses and keep a track on it.
+          </span>
+
+        </div>
+
+        {/* Create Group Button */}
+        <button
+        onClick={() => {setGroupPopup(true)}}
+        className='flex items-center justify-center gap-3 bg-[#2C9986] px-3 rounded-full h-12 text-white shadow-lg'>
+          <img src="/add.svg" alt="" className='w-6' />
+          Create New Group
+        </button>
+
+      </div>
+
+      
+    </div>
+      
     </>
   )
 }
